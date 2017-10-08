fccaa01c99a6fadae78a5fcf54b797d1b2631233
George Codat Tudoran <office@solcreation.ro>
2017-10-08 14:05:21 +0300

    user detail page

--- www/models.py
+++ www/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
 
 # Create your models here.
 class Person(models.Model):
@@ -8,7 +9,11 @@ class Person(models.Model):
 	title = models.CharField(max_length=100)
 	image = models.CharField(max_length=200)
 	slug = models.SlugField(max_length=40, blank=True)
-
+	responsibilities = models.CharField(max_length=1000, default='')
+	bio = models.CharField(max_length=1000, default='')
+	twitter = models.CharField(max_length=40, blank=True)
+	github = models.CharField(max_length=40, blank=True)
+	birthday =models.DateField(default = timezone.now)
 
 	def save(self, *args, **kwargs):
 		if not self.pk:
@@ -16,5 +21,5 @@ class Person(models.Model):
 		super(Person, self).save(*args, **kwargs)
 
 
-	def _str_(self):
+	def __str__(self):
 		return self.name
--- www/urls.py
+++ www/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^members/([a-z0-9-]+)', views.detail, name='detail')
+    url(r'^members/([a-z0-9-]+)/$', views.detail, name='detail')
 ]
